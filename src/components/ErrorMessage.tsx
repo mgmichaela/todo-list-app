c915765ac7f7c96c1f48d3c5ab9d62e4329729a6
mgmichaela <mgmichaelaguzmicka@gmail.com>
2024-02-04 14:13:10 +0100

    add font

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -7,9 +7,10 @@ const ErrorMessage: FC = () => {
             background: '#e4b9be',
             borderRadius: '1.5rem',
             WebkitBoxShadow: '-3px 3px 0px 0px #cf828b',
-            border: '1px solid #38332e'
+            border: '1px solid #38332e',
+            paddingY: '0.25rem'
         }}>
-            <Typography sx={{ fontSize: {xs: '0.7rem', md: '0.85rem'} }}>(´•︵•`) oopsie, you're trying to add an empty task to your to-do list</Typography>
+            <Typography sx={{ fontFamily: 'Special Elite', fontSize: {xs: '0.7rem', md: '0.85rem'} }}>(´•︵•`) oopsie, you're trying to add an empty task to your to-do list</Typography>
         </Container>
     )
 }
